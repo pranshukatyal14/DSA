0ae39788e5e788c702f5c60142ec11ff61a00097
pranshukatyal14 <52360604+pranshukatyal14@users.noreply.github.com>
2021-11-05 16:31:13 +0530

    update

--- graphs/dsuquestions.java
+++ graphs/dsuquestions.java
@@ -23,6 +23,42 @@ public class dsuquestions{
         return sb.toString();
 
     }
+    // no of inlands -2 (locked)
+
+    int par[],suze;
+
+    public List<Integer> numIsland2(int n,int m,int[][]positions){
+        List<Integer>ans=new ArrayList<>();
+        int[][]dir={{1,0},{-1,0},{0,1},{0,-1}};
+        par=new int[n*m];
+        Arrays.fill(par,-1);
+        int count=0;
+        for(int[]p:positions){
+            int i=p[0],j=p[1];
+            if(par[i*m+j]==-1){
+                count++;
+                par[i*m+j]=i*m+j;
+                int p1=findPar(i*m+j);
+                for(int d=0;d<4;d++){
+                    int r=i+dir[d][0];
+                    int c=j+dir[d][1];
+                    if(r>=0 && c>=0 && r<n && c<m && par[r*m+c]!=-1){
+                        int p2=findPar(r*m+c);
+                        if(p1!=p2){
+                            count--;
+                            par[p2]=p1;
+                        }
+                    }
+                }
+                
+                
+            }
+            ans.add(count);
+
+            
+        }
+        return ans;
+    }
 
 
 }
